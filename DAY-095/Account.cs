ed84a483ad868d1f507b64c9f0344afa43656b3f
Ashik Khulal <askhulal@gmail.com>
2023-12-18 13:41:29 -0600

    Updated day 95 materials

---
+++ DAY-095/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp
+{
+    public class Account
+    {
+        double _balance;
+        int _id;
+
+        public Account(int id, double balance) 
+        {
+            this._id = id;
+            this._balance = balance;
+        }
+
+        public int Id { get { return _id; } }
+
+        public void Withdraw(double amount) 
+        {
+            this._balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            this._balance += amount;
+        }
+    }
+}
---
+++ DAY-095/AccountManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp
+{
+    public class AccountManager
+    {
+        Account _fromAccount;
+        Account _toAccount;
+        double _amountToTransfer;
+
+        public AccountManager(Account fromAccount, Account toAccount, double amountToTransfer)
+        {
+            this._fromAccount = fromAccount;
+            this._toAccount = toAccount;
+            this._amountToTransfer = amountToTransfer;
+        }
+
+        public void Transfer()
+        {
+            Console.WriteLine(Thread.CurrentThread.Name + " trying to acquire lock on " + _fromAccount.Id.ToString());
+            lock (_fromAccount) 
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + " acquired lock on " + _fromAccount.Id.ToString());
+                Console.WriteLine(Thread.CurrentThread.Name + " suspended for 1 second");
+                Thread.Sleep(1000);
+                Console.WriteLine(Thread.CurrentThread.Name + " back in action from 1 second sleep and trying to acquire lock on " + _fromAccount.Id.ToString());
+                lock (_toAccount) 
+                {
+                    Console.WriteLine("This code will not execute due to deadlock!");
+                    _fromAccount.Withdraw(_amountToTransfer);
+                    _toAccount.Deposit(_amountToTransfer);
+                }
+
+            }
+        }
+    }
+}
